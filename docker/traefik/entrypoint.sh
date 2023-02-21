ae75ebe09523bdab695f3e986550f4ca098981b2
Toby Griffiths <toby@cubicmushroom.co.uk>
2023-02-21 12:46:05 +0000

    Correct cert creation
    This adds an org name, and replaces the common name with a subject alt
    name, as we need the subject alt name for browsers to accept the
    certificate, and this doesn't work for wildcard domains if the CN
    (common name) is also set.

--- docker/traefik/entrypoint.sh
+++ docker/traefik/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 fqdn="${FQDN?'Missing FQDN'}"
+org="${CERTIFICATE_ORG:-Traefik Dev Proxy}"
 
 if [[ ! -f "/opt/traefik/certs/server.crt" ]] ; then
     echo "${fqdn}"
@@ -8,7 +9,8 @@ if [[ ! -f "/opt/traefik/certs/server.crt" ]] ; then
         -newkey rsa:4096 \
         -keyout /opt/traefik/certs/server.key \
         -out /opt/traefik/certs/server.crt \
-        -subj "/CN=*.${fqdn}"
+        -subj "/O=${org}" \
+        -addext "subjectAltName = DNS:*.${fqdn}"
 
     chmod 644 /opt/traefik/certs/server.crt
     chmod 600 /opt/traefik/certs/server.key
