ce17f38418c4581e662ed55ceb02536fbc0c739d
Toby Griffiths <toby@cubicmushroom.co.uk>
2024-01-16 13:10:31 +0000

    chore: Pass traefkik version to docker compose build cmd
    Also adds reminder to tag the image to `latest` and prune dangling
    images if so desired.

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env sh
 
-docker compose build
-docker compose push
+traefik_version=2.11
+
+TRAEFIK_VERSION=$traefik_version docker compose build \
+    --pull \
+    --no-cache \
+    --push
+
+echo
+echo "**********************************************************************"
+echo "Image 'tobygr/traefik-proxy:$traefik_version' built"
+echo
+echo "If you want to update the latest image run…"
+echo
+echo "$ docker tag tobygr/traefik-proxy:$traefik_version tobygr/traefik-proxy:latest"
+echo
+echo "You may also want to remove the build layers using…"
+echo
+echo "$ docker image prune --force"
+echo "**********************************************************************"
